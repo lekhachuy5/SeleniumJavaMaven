e2a905ae036e391c011a6cce70f25dfe169af421
lekhachuy5 <lekh39@gmail.com>
2023-06-02 17:54:44 +0700

    initial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Properties prop = new Properties();
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("src/config.properties"));
+            prop.load(reader);
+            reader.close();
+        }catch  (FileNotFoundException e){
+            throw new FileNotFoundException("File config.properties is not found in the class path");
+        }
+        WebDriverManager web = WebDriverManager.chromedriver();
+        web.setup();
+        ChromeDriver dr = new ChromeDriver();
+        String url = prop.getProperty("data.url");
+                dr.get(url);
+                dr.quit();
+    }
+}
