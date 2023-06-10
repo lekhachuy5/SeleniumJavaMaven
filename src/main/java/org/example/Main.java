4821527fbb89b746fc0b0b0edf95ccfc8dea5edf
dvminhtan <dvminhtan@gmail.com>
2023-06-10 16:31:39 +0700

    inititla

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.BufferedReader;
@@ -14,20 +16,28 @@ import java.util.Properties;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException {
-        Properties prop = new Properties();
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader("src/config.properties"));
-            prop.load(reader);
-            reader.close();
-        }catch  (FileNotFoundException e){
-            throw new FileNotFoundException("File config.properties is not found in the class path");
-        }
-        WebDriverManager web = WebDriverManager.chromedriver();
-        web.setup();
-        ChromeDriver dr = new ChromeDriver();
-        String url = prop.getProperty("data.url");
-                dr.get(url);
-                dr.quit();
+//        Properties prop = new Properties();
+//        BufferedReader reader;
+//        try {
+//            reader = new BufferedReader(new FileReader("src/config.properties"));
+//            prop.load(reader);
+//            reader.close();
+//        }catch  (FileNotFoundException e){
+//            throw new FileNotFoundException("File config.properties is not found in the class path");
+//        }
+//        WebDriverManager web = WebDriverManager.chromedriver();
+//        web.setup();
+//        ChromeDriver dr = new ChromeDriver();
+//        String url = prop.getProperty("data.url");
+//                dr.get(url);
+//                dr.quit();
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+//        dr.quit();
+        WebElement gmail = driver.findElement(By.xpath("//a[text()='Gmail']"));
+        System.out.println(gmail.getText());
+
+
     }
 }
